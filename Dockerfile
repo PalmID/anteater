f316d6fe39aa578b4cfbaebbbc17819a87032562
leosocy <leosocy@gmail.com>
2019-01-14 21:38:10 +0800

    feat: swagger extension

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,4 @@
-FROM leosocy/opencv:python
-
-# install RobustPalmRoi library
-RUN apk add --no-cache --virtual build-dependencies make cmake linux-headers git && \
-    cd /tmp && git clone https://github.com/jbeder/yaml-cpp.git && \
-    cd yaml-cpp && mkdir -p build && cd build && \
-    cmake -DBUILD_SHARED_LIBS=ON -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF -DYAML_CPP_BUILD_CONTRIB=OFF .. && \
-    make install && \
-    cd /tmp && git clone https://github.com/Leosocy/RobustPalmRoi.git && \
-    cd RobustPalmRoi && mkdir build && cd build && \
-    cmake .. && make install && \
-    cd ../pypackage && python setup.py install && \
-    cd / && rm -rf /tmp/* && \
-    apk del build-dependencies
+FROM leosocy/robustpalmroi:v0.1.1
 
 # install pipenv
 COPY . /usr/src/app
--- app/extensions/swagger.py
+++ app/extensions/swagger.py
@@ -2,8 +2,29 @@
 # Use of this source code is governed by a MIT-style license
 # that can be found in the LICENSE file.
 
-from flasgger import Swagger as BaseSwagger
+import os
+from functools import wraps
+
+from flasgger import Swagger as SwaggerBase
+from flasgger import swag_from
 
 
-class Swagger(BaseSwagger):
-    pass
+class Swagger(SwaggerBase):
+    def init_app(self, app, *args, **kwargs):
+        super().init_app(app, *args, **kwargs)
+        self.docs_root = os.path.join(
+            self.app.root_path, self.app.config["SWAGGER_DOCS_ROOT"]
+        )
+
+    def swag_from(self, doc_name, **swag_kwargs):
+        def decorator(view_func):
+            abs_doc_path = os.path.join(self.docs_root, doc_name)
+            wrap_func = swag_from(specs=abs_doc_path, **swag_kwargs)
+
+            @wraps(view_func)
+            def wrapper(*args, **kwargs):
+                return wrap_func(*args, **kwargs)
+
+            return wrapper
+
+        return decorator
